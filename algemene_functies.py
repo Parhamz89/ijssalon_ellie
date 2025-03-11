71ef17c96d16aab855bf261ec2d98d03d8f55de4
Parham Sheoleslam <parham_95@msn.com>
2025-03-11 14:31:14 +0100

    bestanden reclame.py en algemene_functies.py toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,34 @@
+def mijn_functie_1(a,b):
+    return a + b
+
+totaal = mijn_functie_1(2,2)
+
+print(totaal)
+
+def mijn_functie_2(a, b):
+    return a + b
+
+totaal = mijn_functie_2(12,3)
+
+print(totaal)
+
+def mijn_functie_2(a,b):
+    return a + b
+
+totaal = mijn_functie_2(5,4)
+
+print(totaal)
+
+def mijn_functie_2(a,b):
+    return a + b
+
+totaal = mijn_functie_2(35,1)
+
+print(totaal)
+
+def mijn_functie_2(a,b):
+    return a + b
+
+totaal = mijn_functie_2(2,2)
+
+print(totaal)
